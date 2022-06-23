2867bd7dd8ecf539d8703220e459c3b1c86a01fb
kervangelista <kervangelista1@gmail.com>
2022-06-23 19:56:11 +0800

    Set DEBUG based on environment variables.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pickle import TRUE
 import django_heroku
 import django
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
